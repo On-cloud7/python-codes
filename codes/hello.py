bd093b3c6fe2cc36c5df65a98ffaba48825cb3aa
Your Name <you@example.com>
2025-01-01 18:21:53 +0530

    codes added

---
+++ codes/hello.py
@@ -0,0 +1,19 @@
+print("hello world!")
+
+print("priyanka")
+
+print(3+4)
+
+a=10
+print(a)
+
+print(type(a))
+
+name="shubham"
+print(type(name))
+
+coding=True
+print(type(coding))
+
+num=input("enter a number:")
+print(num)
---
+++ codes/my-first-code.py
@@ -0,0 +1,8 @@
+env=input("enter the cloud platform:")
+if env =="AWS" or env =="aws":
+    print("you are in aws environment")
+else:
+    print("you are in wrong platform")
+
+
+
