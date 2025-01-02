8a7c06c336c074d6dc53859e177e3561f846f245
priyanka pardeshi <140867398+On-cloud7@users.noreply.github.com>
2025-01-02 19:43:43 +0530

    Create calculator.py

---
+++ codes/calculator.py
@@ -0,0 +1,15 @@
+num1 = float(input("Enter the number1:"))
+num2 = float(input("enter the number2:"))
+
+opr = input("enter the operator")
+
+if opr == "+":
+    output=num1+num2
+
+if opr =="-":
+    output=num1-num2
+
+if opr =="*":
+    output=num1*num2
+
+print("calcluation is " , output)
