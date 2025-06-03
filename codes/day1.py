37c115f251389e652f041b4391b0c0b05f8b9457
On-cloud7 <priyanka.p62003@gmail.com>
2025-06-03 18:08:30 +0530

    files added& deleted

---
+++ codes/day1.py
@@ -0,0 +1,10 @@
+print("Hello World!!!")
+#This is a variable
+a= 5
+print(a)
+#This is a variable
+Instructor = "priyanka Pardeshi"
+print("python by",Instructor, sep ='-')
+
+print(5+3)
+print("5+3")
--- codes/my-first-code.py
+++
@@ -1,8 +0,0 @@
-env=input("enter the cloud platform:")
-if env =="AWS" or env =="aws":
-    print("you are in aws environment")
-else:
-    print("you are in wrong platform")
-
-
-
