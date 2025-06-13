ece00808ba9dca7c18a021ed7a8d9685c52945ff
On-cloud7 <priyanka.p62003@gmail.com>
2025-06-13 12:11:58 +0530

    day 2 added

---
+++ day2.py
@@ -0,0 +1,19 @@
+#variable in python
+a=1    #integer values
+print(a)
+
+b = 1.90 #decimal
+print(b)
+
+c="madhav" #word/sentence -use quotes
+
+print(c)
+
+p,q,r = 11,12,13
+print(p,q,r) #assign multiple variables
+
+Rishab= 26
+d= Rishab
+print(d) # declare a variable using a variable
+
+
