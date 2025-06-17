c2eecb28e3e20cd12f729eba842edb475a24614f
On-cloud7 <priyanka.p62003@gmail.com>
2025-06-17 14:12:23 +0530

    ssifnment added

---
+++ Assignment1.py
@@ -0,0 +1,21 @@
+#Assignment1 :
+
+print("Hello World")
+print('Hello World')
+print("you're a good man")
+print('''You're a "good" person'''"\n")
+
+
+#Q1:
+print("Python is fun.")
+print(''' "Quotes" and 'single quotes' can be tricky.''')
+print("\"Quotes\" and 'single quotes ' can be tricky")
+print("Python is fun.\n\"Quotes\" and 'single quotes' can be tricky.""\n")
+
+#Q2:
+
+name ="priyanka"
+age="22"
+city="pune"
+
+print("My name is",name, "im from" , city, "my age is ",age)
