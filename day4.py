17aefc22a4ce505b47cd8a9c96b1329acc0460c2
On-cloud7 <priyanka.p62003@gmail.com>
2025-06-17 19:30:32 +0530

    day4

---
+++ day4.py
@@ -0,0 +1,38 @@
+#type casting in python:
+
+a=1
+print(type(a))
+
+b="1"
+print(type(b))
+
+c=int(b)
+print(type(c))
+
+#all str values can't be casted into numerical type
+
+#name="priyanka"
+#newname = int(name)
+
+print(a+int(b))
+#all numerical type can be cast into str
+mynum = 26
+mynum2 = str(mynum)
+print(type(mynum2))
+
+f1 = 22.5
+f2= int(f1)
+print(type(f1))
+print(type(f2))
+
+f3= 7
+f4= float(f3)
+print(type(f4))
+
+
+#implicit type casting
+var1 = 10
+var2 = 14.7
+var3= var1+var2
+print(var3)
+print(type(var3))
