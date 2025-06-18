78ec2a9e4746e0432c94cae64ae3bfa5e45ff126
On-cloud7 <priyanka.p62003@gmail.com>
2025-06-18 15:10:07 +0530

    day5 added

---
+++ day5.py
@@ -0,0 +1,32 @@
+# #Conditional Statements in python
+ 
+# # 1. if statement
+
+# a = 20
+# b = 29
+# if b>a:
+#     print("b is greater than a ")
+
+# age = int(input("enter the age : "))
+# if age > 19:
+#     print("you are an adult")
+
+# 2. if-else condition
+
+# temp = int(input(" enter the temperature : "))
+# if temp >25:
+#     print("it is hot day")
+# else:
+#     print("it is a cool day ")
+
+# 3. if-elif-else condition:
+
+score = int(input("enter ur score"))
+if score >= 90:
+    print("grade - A")
+elif score >=80:
+    print("grade - C")
+elif score >=70:
+    print("grade - D")
+else:
+    print("grade - D")
