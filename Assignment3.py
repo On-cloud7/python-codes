fdbab5cc8d2fb86d45cfc7fd93c2d431e404b32d
On-cloud7 <priyanka.p62003@gmail.com>
2025-06-18 18:22:31 +0530

    added

--- Assignment3.py
+++ Assignment3.py
@@ -1,19 +1,19 @@
-# #Assignment3 :
+#Assignment3 :
 
-# #condition1 : 4 divisible & 100 not divisible
-# #400 divisible
+#condition1 : 4 divisible & 100 not divisible
+#400 divisible
 
-# #user input
+#user input
 
-# year = int(input("Enter the year(e.e 2024)"))
+year = int(input("Enter the year(e.e 2024)"))
 
-# if (year % 4 == 0 and year %100 !=0) or \
-#       (year %400 == 0):
-#     print(f"{year} is leap year")
-# else:
-#     print("the year is not a leap yr")
+if (year % 4 == 0 and year %100 !=0) or \
+      (year %400 == 0):
+    print(f"{year} is leap year")
+else:
+    print("the year is not a leap yr")
 
-#Q2:
+# Q2:
 
 # predefine username and password
 
@@ -32,4 +32,19 @@ else:
     print("invalid username")
 
 
-    
+
+
+# Q3:
+
+Maths_marks = int(input("enter the marks of maths : "))
+phy_marks = int(input ("Enter the marks of phys: "))
+chem_marks = int(input("Enter the marks of chem"))
+
+if (Maths_marks >= 65 and
+    phy_marks >=55 and
+    chem_marks >=50 and
+    (Maths_marks+phy_marks+chem_marks) >=180 ) or \
+    (Maths_marks+ phy_marks )>=140:
+     print("you re eligible")
+else:
+    print("you are not eligible")
