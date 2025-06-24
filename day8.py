bd50ff3413c74e16b4026ede09607e025f6db320
On-cloud7 <priyanka.p62003@gmail.com>
2025-06-24 21:50:19 +0530

    day8.py added

---
+++ day8.py
@@ -0,0 +1,23 @@
+# #strings in python:
+
+name= "priyanka1"
+print(name)
+print(type(name))
+
+# formatted string - % Operator
+
+name = "priyanka"
+age = 16
+print("my name is %s and i'm %d" % (name ,age))
+
+ # formatted strings - str.format():
+
+name = "priti"
+age = 16
+print("my name is {} and my age is {}" .format(name,age))
+
+# # formatted strings - f strings
+
+name = "sakshi"
+age = 15
+print(f"my name is {name} and im {age}")
