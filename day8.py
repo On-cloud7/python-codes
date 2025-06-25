1e17c7292d63ac2f0dfaf79fb02cf08141c5059e
On-cloud7 <priyanka.p62003@gmail.com>
2025-06-25 17:47:37 +0530

    added

--- day8.py
+++ day8.py
@@ -21,3 +21,40 @@ print("my name is {} and my age is {}" .format(name,age))
 name = "sakshi"
 age = 15
 print(f"my name is {name} and im {age}")
+
+
+# Escape Characters: backslash with chars
+
+
+print(''' "kw-double Quotes" ''') 
+
+print(" \"kw-double Quotes\" ")  # double quotes using \
+
+print(" \'kw-single Quotes\' ") # single quote using \
+
+print("Hello\nWorld")       # new line
+print("Hello\tWorld") 
+
+
+
+# String Operators in Python 
+a = "Hello"
+b = "Python"
+
+print(a+b)      # concatenate 
+print(a*2)      # multiple copies 
+# [] - slice, [:] - range  -- scroll below
+
+if "h" in a:
+    print("Yess")
+else:
+    print("noo")
+
+    
+if "h" not in a:
+    print("Yess")
+else:
+    print("noo") 
+
+print(r"Hello\nWorld")  # Raw string: suppress the escape chars 
+
