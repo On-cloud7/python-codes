942c60211252b0873eccb3f753f2953b5e9d563a
On-cloud7 <priyanka.p62003@gmail.com>
2025-08-01 22:33:05 +0530

    file added

--- day10.py
+++ day10.py
@@ -18,3 +18,51 @@ language = "python"
 for x in language:
     print(x)
 
+# range(start)
+# range(start, stop)
+# range (start, stop, step)
+
+for i in range(5):
+    print(i)
+
+for i in range(5,10):
+    print(i)
+
+for i in range(1,10,2):
+    print(i)
+
+for i in range (5):
+    print(i)
+else:
+    print("for loop ended")
+
+# loop control statements:
+# 1. pass statement:
+
+for i in range(5):
+    #mann nahi hai
+    pass
+
+count = 5
+while count > 0:
+    if count == 3:
+        pass
+else:
+    print(count)
+count -=1
+
+# 2.break statement:
+
+for i in range(10):
+    if i ==5:
+        break
+    print(i)
+
+# 3.continue statement:
+
+for i in range(5):
+    if i == 3:
+        continue
+    print(i)
+
+
