9e5ee6eca3e25938ca85f0b502a8bf67b24921d0
On-cloud7 <priyanka.p62003@gmail.com>
2025-08-01 21:43:21 +0530

    day10.py added

--- day10.py
+++ day10.py
@@ -1,1 +1,20 @@
 #loops - for loop, while,for-each loop
+
+#while loop:
+
+count = 0 
+while count < 4:   #condition
+    print(count)
+    count +=1
+
+count = 5
+while count >0:
+    print(count)
+    count -=1
+else:
+    print("while loop ended")
+
+language = "python"
+for x in language:
+    print(x)
+
