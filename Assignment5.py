d1aaf0a18273d9857e4802bf1f7daf2cc3534cd5
On-cloud7 <priyanka.p62003@gmail.com>
2025-08-02 17:20:04 +0530

    assignment 5 added

--- Assignment5.py
+++ Assignment5.py
@@ -13,11 +13,70 @@
 # 2 print star patterns - using loops:
 
 # Simple Triangle:
-n= 5
+# n= 5
 
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print("*",end = " ")
-    print()
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print("*",end = " ")
+#     print()
 
 
+#inverted Triangle:
+
+# n= 5
+# for i in range (n, 0 ,-1):
+#     for j in range (1 , i+1):
+#         print("*", end = " ")
+#     print()
+
+#3. pyramid pattern :
+n =5
+
+# for i in range(1, n+1):
+#     print(' ' * (n - i), end = "")
+#     print("*" * (2 * i - 1))
+
+# #find the factorial of a numbers:
+
+# def fact(n):
+#     result = 1
+# #     while n >0 :
+# #         result *= n
+# #         n -=1
+# #     return result
+# # print(fact(5))
+
+# #4 Count vowels in a string
+
+# # my_string = "python by priyanka"
+# # vowels = "aeiou"
+# # count = 0
+
+# # for char in my_string:
+# #     if char.lower() in vowels:
+# #         count += 1
+# # print("number of vowels are ", count)
+
+# # find the longest word in a string :
+
+# sentence = "python by priyanka pardeshi"
+# words = sentence.split()
+
+# longest_word = ""
+
+# for i in words:
+#     if len(words) > len(longest_word):
+#         longest_word = words
+# print("the longest word is : ", longest_word)
+
+
+#fibonacci sequence:
+
+def fibonacci(n):
+    a,b = 0,1
+    count = 0
+    while count < n:
+        print(a)
+        a,b = b , a+b
+        count +=1
+print(fibonacci(5))
