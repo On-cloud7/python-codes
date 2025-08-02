178e2db0d102ec9eb45bff2f2383b483349c6cd3
On-cloud7 <priyanka.p62003@gmail.com>
2025-08-02 14:05:19 +0530

    Assignment5 added

---
+++ Assignment5.py
@@ -0,0 +1,23 @@
+# most asked question:
+
+# 1.print the while loop output in same line:
+
+# print("hello", end = " ")
+# print("madhav")
+
+# i = 1
+# while i < 4:
+#     print(f"Hello Madhav {i}",end = " ")
+#     i +=1
+
+# 2 print star patterns - using loops:
+
+# Simple Triangle:
+n= 5
+
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print("*",end = " ")
+    print()
+
+
